735464832d85a29f46ca6dd1fab5cad72d524fc6
Marco De La Pierre <marco.delapierre@gmail.com>
2021-03-15 16:35:27 +0800

    1st commit with backbone

---
+++ README.md
@@ -0,0 +1,12 @@
+## Argonmd
+
+Training code: Molecular Dynamics  
+Lennard Jones liquid with velocity Verlet integrator
+
+Assumptions:
+- Argon chosen as model system
+  - fcc atomic lattice
+  - lattice constant 
+  - LJ parameters
+- 3D cubic box
+  - defined as supercell of the unit cell of Argon
---
+++ argonmd.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+/*
+Training code: Molecular Dynamics
+Lennard Jones liquid with velocity Verlet integrator
+
+Further assumptions:
+- Argon chosen as model system
+  - fcc atomic lattice
+  - lattice constant 
+  - LJ parameters
+- 3D cubic box
+  - defined as supercell of the unit cell of Argon
+
+Marco De La Pierre - 2020
+*/
+
+
+int main() {
+cout<<"Hello World!"<<endl;
+
+
+// define variables
+
+
+// allocate arrays
+
+
+// define structure
+
+
+// initialise velocities
+
+
+
+// big loop: time evolution
+
+// compute forces
+
+// integrate
+
+// update velocities
+
+// print output when required
+
+
+
+// deallocate arrays
+
+
+return 0;
+}
