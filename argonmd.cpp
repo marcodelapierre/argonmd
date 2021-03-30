8d67ddf8cab3820a65f353c019432a27e61f8731
Marco De La Pierre <marco.delapierre@gmail.com>
2021-03-30 21:36:45 +0800

    define structure

--- argonmd.cpp
+++ argonmd.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+//#include <iomanip> // test only
+
 using namespace std;
 
 
@@ -20,16 +22,25 @@ int main() {
 cout<<"Hello World!"<<endl;
 
 // define parameters
-const int box_side = 10; // no of unit cells
+// first three might become editable by cli arguments
+const int box_side = 2; // no of unit cells
+const int nsteps = 1000000;
 const double temp = 50.; // K
+//
 const double eps = 0.34; // nm
 const double sigma = 120.; // K*k_B
 const double mass = 39.9; // amu
 const double step = 0.001; // ps
-const int nsteps = 1000000;
 // argon crystal structure (fcc)
 const double cellpar = 0.5256; // nm
 const int funits = 4;
+const double unitpos[4][3] = {
+  0., 0., 0.,
+  0.5*cellpar, 0.5*cellpar, 0.,
+  0.5*cellpar, 0., 0.5*cellpar,
+  0., 0.5*cellpar, 0.5*cellpar
+};
+
 // physical constants here
 
 
@@ -43,7 +54,23 @@ double** acc = arrayAlloc( natoms, 3);
 
 
 // define structure
-
+int idx;
+int bf = box_side * funits;
+int bbf = box_side * bf;
+for ( int i = 0; i < box_side; i++ ) {
+  for ( int j = 0; j < box_side; j++ ) {
+    for ( int k = 0; k < box_side; k++ ) {
+      for ( int l = 0; l < funits; l++ ) {
+        idx = i * bbf + j * bf + k * funits;
+        pos[idx][0] = cellpar*i + unitpos[l][0];
+        pos[idx][1] = cellpar*j + unitpos[l][1];
+        pos[idx][2] = cellpar*k + unitpos[l][2];
+        //cout << left << setw(8) << pos[idx][0] << setw(8) << pos[idx][1] << setw(8) << pos[idx][2] << endl; // test only
+      }
+      //cout << endl; // test only
+    }
+  }
+}
 
 
 // initialise velocities
@@ -58,7 +85,10 @@ double** acc = arrayAlloc( natoms, 3);
 
 // update velocities
 
-// print output when required
+
+
+
+// compute and print output when required
 
 
 
