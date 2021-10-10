4efc8f06f2cec0527e33b8e1388e535adf9e390b
Marco De La Pierre <marco.delapierre@gmail.com>
2021-10-10 15:20:13 +0800

    serial_one: orthorhombic complete - to be fully tested

--- serial_one_source/argonmd.cpp
+++ serial_one_source/argonmd.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 #include <cmath>
 #include <ctime>
 
@@ -56,7 +57,7 @@ const int box_units[ 3 ] = {
   input_params.box_units[0],
   input_params.box_units[1],
   input_params.box_units[2]
-  }; // no of unit cells per each dimension in the simulation box
+}; // no of unit cells per each dimension in the simulation box
 const int nsteps = input_params.nsteps; // no of time steps in the simulation
 const double temp_ini = input_params.temp_ini; // K [117.7: datum from LAMMPS LJ example]
 const int nneighupd = input_params.nneighupd; // update neighbour list every these steps [from LAMMPS LJ example]
@@ -76,18 +77,18 @@ const double boxlen[ 3 ] = {
   cellpar * box_units[0],
   cellpar * box_units[1],
   cellpar * box_units[2]
-  }; // affected by input parameters
+}; // affected by input parameters
 const double boxhalf[ 3 ] = {
   0.5 * boxlen[0],
   0.5 * boxlen[1],
   0.5 * boxlen[2]
-  }; // affected by input parameters
+}; // affected by input parameters
 const double unitpos[ funits * 3 ] = {
   0., 0., 0.,
   0.5*cellpar, 0.5*cellpar, 0.,
   0.5*cellpar, 0., 0.5*cellpar,
   0., 0.5*cellpar, 0.5*cellpar
-  };
+};
 //
 // Some physical constants here
 const double k_B = 8.617343e-05; // eV/K
@@ -254,36 +255,62 @@ InputParams get_input_params( const int argc, char** argv )
   InputParams input_params;
 
   if ( argc > 1 ) {
-    input_params.box_units[0] = atoi(argv[1]);
-    input_params.box_units[1] = atoi(argv[1]);
-    input_params.box_units[2] = atoi(argv[1]);
+// allowing requests for orthorhombic boxes
+    char input_string[ 63 ];
+    char* token_string;
+    int count = 0;
+    int box_units[ 3 ] = { 0, 0, 0 };
+    strcpy ( input_string, argv[1] );
+    token_string = strtok( input_string, "," );
+    while ( token_string != NULL ) {
+      box_units[count++] = atoi( token_string );
+      token_string = strtok( NULL, "," );
+    }
+    if ( count == 1 ) {
+      input_params.box_units[0] = box_units[0];
+      input_params.box_units[1] = box_units[0];
+      input_params.box_units[2] = box_units[0];
+    } else if ( count == 2 ) {
+      input_params.box_units[0] = box_units[0];
+      input_params.box_units[1] = box_units[0];
+      input_params.box_units[2] = box_units[1];
+    } else if ( count == 3 ) {
+      input_params.box_units[0] = box_units[0];
+      input_params.box_units[1] = box_units[1];
+      input_params.box_units[2] = box_units[2];
+    } else {
+      input_params.box_units[0] = 5;
+      input_params.box_units[1] = 5;
+      input_params.box_units[2] = 5;
+    }
   } else {
     input_params.box_units[0] = 5;
     input_params.box_units[1] = 5;
     input_params.box_units[2] = 5;
   }
+
   if ( argc > 2 ) {
-    input_params.nsteps = atoi(argv[2]);
+    input_params.nsteps = atoi( argv[2] );
   } else {
     input_params.nsteps = 10000;
   }
   if ( argc > 3 ) {
-    input_params.temp_ini = atof(argv[3]);
+    input_params.temp_ini = atof( argv[3] );
   } else {
     input_params.temp_ini = 10.;
   }
   if ( argc > 4 ) {
-    input_params.nneighupd = atoi(argv[4]);
+    input_params.nneighupd = atoi( argv[4] );
   } else {
     input_params.nneighupd = 20;
   }
   if ( argc > 5 ) {
-    input_params.nthermo = atoi(argv[5]);
+    input_params.nthermo = atoi( argv[5] );
   } else {
     input_params.nthermo = 1000;
   }
   if ( argc > 6 ) {
-    input_params.ndump = atoi(argv[6]);
+    input_params.ndump = atoi( argv[6] );
   } else {
     input_params.ndump = 0;
   }
