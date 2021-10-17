6617319968a4d536e8852fe364750836188e86ec
Marco De La Pierre <marco.delapierre@gmail.com>
2021-10-17 13:50:20 +0800

    serial 2x: performance tweak to pos_vel funcs

--- serial_multi_sources/pos_vel.cpp
+++ serial_multi_sources/pos_vel.cpp
@@ -5,18 +5,22 @@
 // Note that this implies 3D PBC
 void check_pbc( double* pos, const int natoms, const double* const boxlen ) 
 {
+  const double boxlen0 = boxlen[0];
+  const double boxlen1 = boxlen[1];
+  const double boxlen2 = boxlen[2];
+
   for ( int i = 0; i < natoms; i++ ) {
     double x = pos[ 3 * i + 0 ];
-    if ( x >= boxlen[0] ) { x -= boxlen[0]; pos[ 3 * i + 0 ] = x; }
-    if ( x < 0. )      { x += boxlen[0]; pos[ 3 * i + 0 ] = x; }
+    if ( x >= boxlen0 ) { x -= boxlen0; pos[ 3 * i + 0 ] = x; }
+    if ( x < 0. )      { x += boxlen0; pos[ 3 * i + 0 ] = x; }
   
     double y = pos[ 3 * i + 1 ];
-    if ( y >= boxlen[1] ) { y -= boxlen[1]; pos[ 3 * i + 1 ] = y; }
-    if ( y < 0. )      { y += boxlen[1]; pos[ 3 * i + 1 ] = y; }
+    if ( y >= boxlen1 ) { y -= boxlen1; pos[ 3 * i + 1 ] = y; }
+    if ( y < 0. )      { y += boxlen1; pos[ 3 * i + 1 ] = y; }
   
     double z = pos[ 3 * i + 2 ];
-    if ( z >= boxlen[2] ) { z -= boxlen[2]; pos[ 3 * i + 2 ] = z; }
-    if ( z < 0. )      { z += boxlen[2]; pos[ 3 * i + 2 ] = z; }
+    if ( z >= boxlen2 ) { z -= boxlen2; pos[ 3 * i + 2 ] = z; }
+    if ( z < 0. )      { z += boxlen2; pos[ 3 * i + 2 ] = z; }
   }
 
   return;
@@ -29,25 +33,29 @@ void update_pos_pbc( double* pos, double* posraw,
     const double dt, const double forc_hdtsq_scale, 
     const double imass, const double* const boxlen ) 
 {
+  const double boxlen0 = boxlen[0];
+  const double boxlen1 = boxlen[1];
+  const double boxlen2 = boxlen[2];
+
   for ( int i = 0; i < natoms; i++ ) {
     double dx = vel[ 3 * i + 0 ] * dt + forc[ 3 * i + 0 ] * forc_hdtsq_scale * imass;
     double x = pos[ 3 * i + 0 ] + dx;
-    if ( x >= boxlen[0] ) { x -= boxlen[0]; }
-    if ( x < 0. )      { x += boxlen[0]; }
+    if ( x >= boxlen0 ) { x -= boxlen0; }
+    if ( x < 0. )      { x += boxlen0; }
     pos[ 3 * i + 0 ] = x;
     posraw[ 3 * i + 0 ] += dx;
   
     double dy = vel[ 3 * i + 1 ] * dt + forc[ 3 * i + 1 ] * forc_hdtsq_scale * imass;
     double y = pos[ 3 * i + 1 ] + dy;
-    if ( y >= boxlen[1] ) { y -= boxlen[1]; }
-    if ( y < 0. )      { y += boxlen[1]; }
+    if ( y >= boxlen1 ) { y -= boxlen1; }
+    if ( y < 0. )      { y += boxlen1; }
     pos[ 3 * i + 1 ] = y;
     posraw[ 3 * i + 1 ] += dy;
   
     double dz = vel[ 3 * i + 2 ] * dt + forc[ 3 * i + 2 ] * forc_hdtsq_scale * imass;
     double z = pos[ 3 * i + 2 ] + dz;
-    if ( z >= boxlen[2] ) { z -= boxlen[2]; }
-    if ( z < 0. )      { z += boxlen[2]; }
+    if ( z >= boxlen2 ) { z -= boxlen2; }
+    if ( z < 0. )      { z += boxlen2; }
     pos[ 3 * i + 2 ] = z;
     posraw[ 3 * i + 2 ] += dz;
   }
